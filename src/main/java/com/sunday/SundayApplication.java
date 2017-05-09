ecefdab1d43e482ae55b3272dcb9990e9dc2c05a
novot <novot@nate.com>
2017-05-09 12:38:37 +0900

    프로젝트 초기화

---
+++ src/main/java/com/sunday/SundayApplication.java
@@ -0,0 +1,12 @@
+package com.sunday;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SundayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SundayApplication.class, args);
+	}
+}
